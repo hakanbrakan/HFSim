195f0a7dc01eb6e3c2cc259c7545f02feb452146
inger <inger@192.168.1.2>
2017-11-03 21:02:07 +0100

    Lite större WalkingMan

--- src/main/java/se/frihak/hfsim/simobjects/WalkingMan.java
+++ src/main/java/se/frihak/hfsim/simobjects/WalkingMan.java
@@ -1,11 +1,12 @@
 package se.frihak.hfsim.simobjects;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
 public class WalkingMan extends GameObject {
-	private static final int WIDTH = 3;
-	private static final int HEIGHT = 3;
+	private static final int WIDTH = 5;
+	private static final int HEIGHT = 5;
 
 	public WalkingMan(SpecGameObject enSpec) {
 		super(1, 1, ID.WalkingMan);
@@ -20,8 +21,8 @@ public class WalkingMan extends GameObject {
 
 	@Override
 	public void render(Graphics g) {
-		// TODO Auto-generated method stub
-
+		g.setColor(Color.BLUE);
+		g.fillRect((int) x, (int) y, WIDTH, HEIGHT);
 	}
 
 	@Override
